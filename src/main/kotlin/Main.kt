3309760428024346fdc658467189e7f4f38d82ce
Yigit Ozgumus <yigitozgumus1@gmail.com>
2022-12-01 11:16:42 +0300

    Solution for day 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
-import days.Day1
-import days.solvePart1
+import days.solveDay1
 
 fun main() {
-	val day1 = Day1()
-	day1.solvePart1()
+	solveDay1()
 }
--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -2,9 +2,30 @@ package days
 
 import utils.SolutionData
 
-class Day1: SolutionData(inputFile = "inputs/day1.txt") {
+fun solveDay1() {
+	Day1().run {
+		solvePart1()
+		solvePart2()
+	}
 }
 
+class Day1: SolutionData(inputFile = "inputs/day1.txt") {
+	val processedData = rawData.flatMapIndexed { index: Int, s: String ->
+		when {
+			index == 0 || index == rawData.lastIndex -> listOf(index)
+			s.isEmpty() -> listOf(index - 1, index + 1)
+			else -> emptyList()
+		}
+	}.windowed(size = 2, step = 2) { (from, to) -> rawData.slice(from..to) }
+}
+
+
 fun Day1.solvePart1() {
-	println(rawData)
+	val listOfSums = processedData.map { it.map { it -> it.toInt() }.sum() }
+	println(listOfSums.max())
+}
+
+fun Day1.solvePart2() {
+	val listOfSums = processedData.map { it.map { it -> it.toInt() }.sum() }
+	println(listOfSums.sortedDescending().take(3).sum())
 }
