97b95c60f450bc54e20215eb9ca557616944cb0e
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2022-12-01 10:25:27 +0300

    update base solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,7 @@
+import days.Day1
+import days.solvePart1
+
 fun main() {
+	val day1 = Day1()
+	day1.solvePart1()
 }
---
+++ src/main/kotlin/days/Day1.kt
@@ -0,0 +1,10 @@
+package days
+
+import utils.SolutionData
+
+class Day1: SolutionData(inputFile = "inputs/day1.txt") {
+}
+
+fun Day1.solvePart1() {
+	println(rawData)
+}
