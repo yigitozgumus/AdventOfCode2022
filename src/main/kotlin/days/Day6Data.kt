fd78cdbc9ee4f1711e031078f683eb44c84ced6b
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2022-12-06 08:14:01 +0300

    Solve Day 6

--- src/main/kotlin/days/Day6Data.kt
+++ src/main/kotlin/days/Day6Data.kt
@@ -2,12 +2,20 @@ package days
 
 import utils.SolutionData
 
-class Day6Data: SolutionData(inputFile = "inputs/day6.txt")
+class Day6Data: SolutionData(inputFile = "inputs/day6.txt") {
+	val processed = rawData.first()
+}
 
 fun Day6Data.solvePart1() {
-
+	processed
+		.windowed(size = 4, step = 1)
+		.indexOfFirst { it.toSet().size == 4 }
+		.also { println(it + 4) }
 }
 
 fun Day6Data.solvePart2() {
-
+	processed
+		.windowed(size = 14, step = 1)
+		.indexOfFirst { it.toSet().size == 14 }
+		.also { println(it + 14) }
 }
